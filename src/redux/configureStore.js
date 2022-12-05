97d8c875fc474bc3fb8de151e943d210ce377714
Aneal Laryea <aneallaryea100@gmail.com>
2022-12-05 10:26:11 +0000

    Add components

---
+++ src/redux/configureStore.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+
+export const store = configureStore({
+  reducer: {
+    // Add the generated reducer as a specific top-level slice
+  },
+});
