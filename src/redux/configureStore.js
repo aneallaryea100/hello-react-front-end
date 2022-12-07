830290eb3a0deb6d464f91e3fed862ce92bac545
Aneal Laryea <aneallaryea100@gmail.com>
2022-12-07 06:18:07 +0000

    Display greetings from rails api

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,12 @@
+/* eslint-disable import/prefer-default-export */
 import { configureStore } from '@reduxjs/toolkit';
-
+import greetingsSlice from './greetings/greetings';
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
+    greetings: greetingsSlice,
   },
 });
+
+export default store;
