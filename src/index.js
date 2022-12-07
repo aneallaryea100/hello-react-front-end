0683a13358380e0006c0e23472204e14af6b2368
Aneal Laryea <aneallaryea100@gmail.com>
2022-12-07 06:28:22 +0000

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
+import { store } from './redux/configureStore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
